f89637397649269db4210b8071cb64aa044a05af
Duman Khuangan <duman2k@gmail.com>
2020-06-25 14:37:38 +0600

    created default controller, view and layout page

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Video School';
+?>
+
+<div class="site-index">
+    <h1>Video School</h1>
+
+    Welcome to the Video Paradise :)
+</div>
--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
 <?php
+
+    define ('YII_DEBUG', true);
+
     require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
     $config = require __DIR__.'/../config/web.php';
 
